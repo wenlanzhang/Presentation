e4c99ebe45b2de54dba237512a848d4c6019aef1
Zhang.Wenlan <60964139+wenlanzhang@users.noreply.github.com>
2023-04-01 16:56:59 +0100

    Slides update

---
+++ Gitconfig.R
@@ -0,0 +1,18 @@
+# Github configureation
+
+# Check https://andrewmaclachlan.github.io/CASA0005repo/git-github-and-rmarkdown.html
+
+# Token: Classical
+
+
+library(usethis)
+create_github_token()
+
+
+
+
+
+library(gitcreds)
+gitcreds_set()
+
+
